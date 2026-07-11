4a33fb26e4ad66de2f1484da28b4d311d3b4f0e0
francoism90 <francoism90@users.noreply.github.com>
2026-07-11 22:49:48 +0200

    Call installPodmanQuadlet and display success

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -8,10 +8,11 @@ use Foxws\Podman\Concerns\InteractsWithPodmanQuadlet;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 
+use function Laravel\Prompts\info;
 use function Laravel\Prompts\select;
 use function Laravel\Prompts\text;
 
-#[AsCommand(name: 'podman:install')]
+#[AsCommand(name: "podman:install")]
 class InstallCommand extends Command
 {
     use InteractsWithPodmanQuadlet;
@@ -21,20 +22,28 @@ class InstallCommand extends Command
         {--replace : Replace the service if it already exists}
     ';
 
-    public $description = 'Install a service for the application to run with Podman';
+    public $description = "Install a service for the application to run with Podman";
 
     public function handle(): int
     {
         $application = text(
-            label: 'Enter the application name',
-            placeholder: 'my-app',
+            label: "Enter the application name",
+            placeholder: "my-app",
         );
 
         $service = select(
-            label: 'Select a service to install',
+            label: "Select a service to install",
             options: $this->getPodmanQuadletServices(),
             required: true,
         );
+
+        $this->installPodmanQuadlet(
+            application: $application,
+            service: $service,
+            replace: $this->option("replace"),
+        );
+
+        info("Service {$service} installed for application {$application}");
 
         return self::SUCCESS;
     }

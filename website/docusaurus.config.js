6dd0b3d67e7bd33f43e77d9fdd88b6c2147d48a2
francoism90 <francoism90@users.noreply.github.com>
2026-08-31 16:11:40 +0000

    chore: add pnpm workspace configuration to allow builds for core-js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,12 @@ const config = {
 
   onBrokenLinks: 'throw',
   onBrokenAnchors: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   i18n: {
     defaultLocale: 'en',
